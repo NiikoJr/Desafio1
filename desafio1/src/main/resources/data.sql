de7310caadc7919f2d5b3cb75dc685e6548c2bc0
Niiko Jr <nalas907@gmail.com>
2025-03-02 03:48:54 -0600

    archivos sql

---
+++ desafio1/src/main/resources/data.sql
@@ -0,0 +1,15 @@
+INSERT INTO Materia (nombre) VALUES
+('POO'),
+('DWF'),
+('PHP'),
+('SDR'),
+('BDD');
+
+INSERT INTO Alumno (nombre, apellido, id_materia) VALUES
+('Nicolas', 'Alas', 1),
+('Juan', 'Gómez', 2),
+('Isabel', 'Marroquin', 3),
+('Olivia', 'Luna', 4),
+('Ernesto', 'Landaverde', 5);
+
+
